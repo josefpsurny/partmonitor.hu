4ff5ed414cfbdfaca484b0d4a6cb9f247546cb25
Michal Skop <michal.skop@kohovolit.eu>
2022-03-14 01:26:00 +0100

    yml working dir

--- vue/test-2022/dist/_nuxt/static/1647214479/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fheroes","\u002Ftest","\u002Fquestion\u002F1","\u002Fcomparison","\u002Fquestion\u002F2","\u002Fme","\u002Fextra","\u002Fcompare","\u002Fresults","\u002Fquestion\u002F3","\u002Fquestion\u002F4"]})
