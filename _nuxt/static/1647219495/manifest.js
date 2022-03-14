fbfee4296c4d7b96a2e4ff39e8f868092e0aef51
josefpsurny <josefpsurny@users.noreply.github.com>
2022-03-14 11:44:42 +0000

    deploy: bef11c4fdb0dd2b4a3a1ae1da05f21fa7697fcc0

--- _nuxt/static/1647219495/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fquestion\u002F1","\u002Fcompare","\u002Fextra","\u002Fheroes","\u002Fme","\u002Fresults","\u002Ftest","\u002Fcomparison","\u002Fquestion\u002F2","\u002Fquestion\u002F3","\u002Fquestion\u002F4"]})
