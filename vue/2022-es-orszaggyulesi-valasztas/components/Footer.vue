73155b3cce66c2661916260a97b9dfa253e58399
Michal Skop <michal.skop@kohovolit.eu>
2022-03-27 01:12:01 +0100

    logos path

--- vue/2022-es-orszaggyulesi-valasztas/components/Footer.vue
+++ vue/2022-es-orszaggyulesi-valasztas/components/Footer.vue
@@ -7,9 +7,9 @@
 
         </div>
         <div class="col-md-6 text-center">
-          <a href="https://k-monitor.hu" title="K-Monitor.hu" id="footer-k-monitor-logo" target="_blank"><img src='/kmonitor.png' alt="logo k-monitor.hu"></a><br/>
-            <a href="http://kohovolit.eu/" title="KohoVolit.eu" id="footer-kohovolit-logo" target="_blank"><img src='/kohovolit.png' alt="logo kohovolit.eu"></a><br/>
-            <a href="https://cesko.digital" title="Cesko.digital" id="footer-cesko-digital-logo" target="_blank" ><img src='/cesko.digital.svg' alt="logo cesko.digital" width="311"></a><br/>
+          <a href="https://k-monitor.hu" title="K-Monitor.hu" id="footer-k-monitor-logo" target="_blank"><img src='kmonitor.png' alt="logo k-monitor.hu"></a><br/>
+            <a href="http://kohovolit.eu/" title="KohoVolit.eu" id="footer-kohovolit-logo" target="_blank"><img src='kohovolit.png' alt="logo kohovolit.eu"></a><br/>
+            <a href="https://cesko.digital" title="Cesko.digital" id="footer-cesko-digital-logo" target="_blank" ><img src='cesko.digital.svg' alt="logo cesko.digital" width="311"></a><br/>
               <!-- <p class="lead black">Volební kalkulačky <sup><a href="https://isdv.upv.cz/webapp/resdb.print_detail.det?pspis=OZ/542865&plang=CS" target="_blank">®</a></sup> pro Vás už od roku 2006 vytváří občanské sdružení <a href="http://kohovolit.eu" target="_blank">KohoVolit.eu</a></p> -->
             <hr/>
               <p class="black">Creative Commons BY NC 4.0 | <a href="http://kohovolit.eu" id="footer-kohovolit-licence">KohoVolit.eu</a>, <a href="https://kmonitor.hu">K-Monitor</a>, <a href="https://cesko.digital">Česko.Digital</a> 2022</p>
