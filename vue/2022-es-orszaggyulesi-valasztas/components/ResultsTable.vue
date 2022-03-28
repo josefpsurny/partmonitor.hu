fa69a3fdbb9c00aa06e5b3cbb3cfc3d53c09f850
Michal Skop <michal.skop@kohovolit.eu>
2022-03-28 15:10:35 +0200

    updated version

--- vue/2022-es-orszaggyulesi-valasztas/components/ResultsTable.vue
+++ vue/2022-es-orszaggyulesi-valasztas/components/ResultsTable.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="container">
-      <h4 class="p-2">Az én meccsem:</h4>
+      <h4 class="p-2">Az én eredményeim:</h4>
 
       <ul class="list-group" v-for="(result, index) in results" :key="index">
         <!-- <li class="list-group-item d-flex" @click="showCandidate(result.info.id)"> -->
